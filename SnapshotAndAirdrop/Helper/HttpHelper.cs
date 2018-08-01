ee929170e002f9a773eb5745f211de98d89b60f9
wowoyinwei <82604458@qq.com>
2018-08-01 15:39:28 +0800

    提交httphelper

---
+++ SnapshotAndAirdrop/Helper/HttpHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotAndAirdrop.Helper
+{
+    class HttpHelper
+    {
+        public static string MakeRpcUrlPost(string url, string method, out byte[] data, params MyJson.IJsonNode[] _params)
+        {
+            var json = new MyJson.JsonNode_Object();
+            json["id"] = new MyJson.JsonNode_ValueNumber(1);
+            json["jsonrpc"] = new MyJson.JsonNode_ValueString("2.0");
+            json["method"] = new MyJson.JsonNode_ValueString(method);
+            StringBuilder sb = new StringBuilder();
+            var array = new MyJson.JsonNode_Array();
+            for (var i = 0; i < _params.Length; i++)
+            {
+
+                array.Add(_params[i]);
+            }
+            json["params"] = array;
+            data = System.Text.Encoding.UTF8.GetBytes(json.ToString());
+            return url;
+        }
+        public static string HttpGet(string url)
+        {
+            WebClient wc = new WebClient();
+            return wc.DownloadString(url);
+        }
+        public static string HttpPost(string url, byte[] data)
+        {
+            WebClient wc = new WebClient();
+            wc.Headers["content-type"] = "text/plain;charset=UTF-8";
+            byte[] retdata;
+            try
+            {
+                retdata = wc.UploadData(url, "POST", data);
+            }
+            catch (Exception e)
+            {
+                System.Threading.Thread.Sleep(100);
+                return HttpPost(url, data);
+            }
+            return System.Text.Encoding.UTF8.GetString(retdata);
+        }
+
+        public static async Task<string> HttpGetAsyncy(string url)
+        {
+            WebClient wc = new WebClient();
+            return await wc.DownloadStringTaskAsync(url);
+        }
+        public static async Task<string> HttpPostAsyncy(string url, byte[] data)
+        {
+            WebClient wc = new WebClient();
+            wc.Headers["content-type"] = "text/plain;charset=UTF-8";
+            byte[] retdata = await wc.UploadDataTaskAsync(url, "POST", data);
+            return System.Text.Encoding.UTF8.GetString(retdata);
+        }
+    }
+}
---
+++ SnapshotAndAirdrop/Helper/ToolHelper.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapshotAndAirdrop.Helper
+{
+    class ToolHelper
+    {
+        public static string RemoveUndefinedParams(MyJson.JsonNode_Object jo)
+        {
+            string[] keys = new string[jo.Keys.Count];
+            jo.Keys.CopyTo(keys, 0);
+
+            for (var i = 0;i< keys.Length; i++)
+            {
+                var key = keys[i];
+                var j = jo.GetDictItem(key);
+                if (j.IsNull())
+                {
+                    jo.Remove(key);
+                }
+                else if (j.ToString() == "0")
+                {
+                    jo.Remove(key);
+                }
+                else if (key[0] == '_' && key[1] == '_')
+                {
+                    jo.Remove(key);
+                }
+            }
+
+            return jo.ToString();
+        }
+
+
+        public static string RemoveRedundantParams(MyJson.JsonNode_Object jo)
+        {
+            string[] keys = new string[jo.Keys.Count];
+            jo.Keys.CopyTo(keys, 0);
+
+            for (var i = 0; i < keys.Length; i++)
+            {
+                var key = keys[i];
+                var j = jo.GetDictItem(key);
+                if (key[0] == '_' && key[1] == '_')
+                {
+                    jo.Remove(key);
+                }
+            }
+
+            return jo.ToString();
+        }
+    }
+}
